8d9447b6df7a1b297ab38d2b2682ad4847d58c3a
Toby Cm <toby@vsus.app>
2025-07-19 11:01:36 -0700

    remove prefix command

--- commands/index.ts
+++ commands/index.ts
@@ -3,13 +3,11 @@ import Command from "modules/command";
 import autodownload from "./autodownload";
 import download from "./download";
 import ping from "./ping";
-import prefix from "./prefix";
 import removeautodownload from "./removeautodownload";
 import services from "./services";
 
 const commands: Command[] = [
   ping, // add your commands here
-  prefix,
   autodownload,
   removeautodownload,
   services,
